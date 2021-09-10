774eb43309bf94ecce368c82a6bc3ec05f1a23e4
WLRFernando <wlrfernando@outlook.com>
2021-09-10 11:00:03 +0530

    add circleci config

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+# exit on error
+set -e
+REGION=$1; EnvironmentName=$2;
+
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID} --profile default
+aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY} --profile default
+aws configure set default.region ${REGION} --profile default
+
+#function for cloudformation deploy
+function deploy() {
+    TEMPLATE_FILE=$1; shift
+    STACKNAME=$1; shift
+
+    ( set -x ; aws cloudformation deploy \
+      --region "${REGION}" \
+      --template-file "${TEMPLATE_FILE}" \
+      --stack-name "${STACKNAME}" \
+      --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+      --no-fail-on-empty-changeset \
+      --parameter-overrides "$@" )
+
+    echo ""
+}
+
+
+deploy VPC.yaml websvr EnvironmentName="${EnvironmentName}" --no-fail-on-empty-changeset
+
+deploy EC2.yaml websvrEC2 --no-fail-on-empty-changeset
+
